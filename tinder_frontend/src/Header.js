793ff50861b10f0598ce581efd752da5470e64d4
MugenAkmal <md.akmal260896@gmail.com>
2022-01-26 21:30:23 +0800

    Desiging Header

---
+++ tinder_frontend/src/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
+import TinderImage from './tinder.png';
+
+function Header() {
+    return(
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+            <img className="header__logo"
+            src = {TinderImage}/>
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+        </div>
+    )
+}
+
+export default Header;
